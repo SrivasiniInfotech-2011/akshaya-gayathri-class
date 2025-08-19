a4585cac6f94a2cf03023591121e0c36e6c81ae5
Srivatsan S <seshadri.srivatsan@gmail.com>
2025-08-19 09:48:39 +0530

    Completed Access Specifier and Inheritence

--- CharpAdvance.Models/Customer.cs
+++ CharpAdvance.Models/Customer.cs
@@ -27,4 +27,26 @@ public class Customer(string name)
     {
         return $"Id: {Id}, Name: {Name}, Email:{Email}, Date Of Birth: {DateOfBirth.GetValueOrDefault().ToString("dd-MM-yyyy")}";
     }
+
+    internal void SetName(string name)
+    {
+        _name = name;
+    }
+
+    protected internal void SetNamePI(string name)
+    {
+        _name = name;
+    }
+}
+
+public class ProtectedCustomer(string name) : Customer(name)
+{
+  public void SetNameProtected(string name)
+  {
+      // Cannot access the SetName method of Customer class as it is private
+      // base.SetName(name);
+      // Can access the SetNamePI method as it is protected internal
+      SetNamePI(name);
+      SetName(name); // This will work as SetName is internal in the same assembly
+    }
 }
