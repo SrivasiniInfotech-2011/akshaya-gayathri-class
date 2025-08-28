4560046d26794ef02a938e24a632c021432e8d14
Srivatsan S <seshadri.srivatsan@gmail.com>
2025-08-28 10:15:30 +0530

    Completed the major portions of the class

---
+++ CSharpAdvanceTraining/EmployeeClass.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace CSharpTraining
+{
+    enum Options
+    {
+        Yes,
+        No
+    }
+    public class EmployeeClass
+    {
+        private int _id;
+        private string _firstName;
+        private string _middleName;
+        private string _lastName;
+        private DateTime _dob;
+        private DateTime _doj;
+
+        public int EmployeeId
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string EmployeeFirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string EmployeeMiddleName
+        {
+            get { return _middleName; }
+            set { _middleName = value; }
+        }
+
+        public string EmployeeLastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public DateTime EmployeeDateOfBirth
+        {
+            get { return _dob; }
+            set { _dob = value; }
+        }
+
+        public DateTime EmployeeDateOfJoining
+        {
+            get { return _doj; }
+            set { _doj = value; }
+        }
+
+        public string EmployeeFullName
+        {
+            get { return $"{_firstName} {_middleName} {_lastName}"; }
+        }
+
+        public EmployeeClass()
+        {
+
+        }
+
+        public EmployeeClass(int id, string firstName, string middleName, string lastName, DateTime dob, DateTime doj)
+        {
+            this.EmployeeId = id;
+            this.EmployeeFirstName = firstName;
+            this.EmployeeMiddleName = middleName;
+            this.EmployeeLastName = lastName;
+            this.EmployeeDateOfBirth = dob;
+            this.EmployeeDateOfJoining = doj;
+        }
+
+        ~EmployeeClass()
+        {
+            Console.WriteLine("Program Execution Completed");
+        }
+
+        public string GetEmployeeDetailString()
+        {
+            return $"Employee Id: {this.EmployeeId}\nEmployee First Name: {this.EmployeeFirstName}\nEmployee Middle Name: {this.EmployeeMiddleName}\nEmployee Last Name: {this.EmployeeLastName}\nEmployee Full Name:{this.EmployeeFullName}\nEmployee Date of Birth:{this.EmployeeDateOfBirth}\nEmployee Date Of Joining: {this.EmployeeDateOfJoining}";
+        }
+
+    }
+
+    public class EmployeeMain
+    {
+        public static void Main(string[] args)
+        {
+            int _id = 0;
+            EMPLOYEE_INITIALIZE:
+                _id++;
+                Console.Write("Enter the First Name:");
+                string firstName = Console.ReadLine();
+                Console.Write("Enter the Middle Name:");
+                string middleName = Console.ReadLine();
+                Console.Write("Enter the Last Name:");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter the Date Of Birth:");
+                DateTime.TryParse(Console.ReadLine(), out DateTime dob);
+                Console.Write("Enter the Date Of Joining:");
+                DateTime.TryParse(Console.ReadLine(), out DateTime doj);
+
+                EmployeeClass employee = new EmployeeClass(_id, firstName, middleName, lastName, dob, doj);
+                Console.WriteLine("Following are the Employee Details:");
+                Console.WriteLine(employee.GetEmployeeDetailString());
+
+                Console.WriteLine("Do you want to enter another Employee? Type Yes or No:");
+                Enum.TryParse(typeof(Options), Console.ReadLine(), out object option);
+                if (((Options)option) == Options.Yes)
+                    goto EMPLOYEE_INITIALIZE;
+
+        }
+    }
+}
---
+++ CSharpAdvanceTraining/Enums.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    enum Colors :int
+    {
+        Red = 1213,
+        Blue = 1314,
+        Green = 1516
+    }
+    enum Choice
+    {
+        Add = 1,
+        Sub = 2,
+        Mul = 3,
+        Div = 4
+    }
+    //class Enums
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        //int inputColor = int.Parse(Console.ReadLine());
+    //        //if (inputColor == (int)Colors.Red)
+    //        //    Console.WriteLine("The selected color is: {0}", Colors.Red.ToString());
+    //        //else if (inputColor == (int)Colors.Green)
+    //        //    Console.WriteLine("The selected color is: {0}", Colors.Green.ToString());
+    //        //else if (inputColor == (int)Colors.Blue)
+    //        //    Console.WriteLine("The selected color is: {0}", Colors.Blue.ToString());
+    //        //else
+    //        //    Console.WriteLine("Please select a valid color");
+
+    //        string inputColor = Console.ReadLine();
+    //       Enum.TryParse(enumType: typeof(Colors), value: inputColor, ignoreCase: true, out object color);
+
+    //        switch ((Colors)color)
+    //        {
+    //            case Colors.Red:
+    //                Console.WriteLine("The selected color is: {0}", Colors.Red.ToString());
+    //                break;
+    //            case Colors.Blue:
+    //                Console.WriteLine("The selected color is: {0}", Colors.Blue.ToString());
+    //                break;
+    //            case Colors.Green:
+    //                Console.WriteLine("The selected color is: {0}", Colors.Green.ToString());
+    //                break;
+    //            default:
+    //                Console.WriteLine("Please select a valid color");
+    //                break;
+    //        }
+    //    }
+    //}
+
+    class Calculator
+    {
+        int a;
+        int b;
+
+        public Calculator()
+        {
+            this.a = 0;
+            this.b = 20;
+        }
+
+        public Calculator(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        ~Calculator()
+        {
+            Console.WriteLine("Program Execution Competed!");
+        }
+        public void Calculate(Choice choice)
+        {
+            switch (choice)
+            {
+                case Choice.Add:
+                    Console.WriteLine("The Sum of {0} and {1} is : {2}", a, b, a + b);
+                    break;
+                case Choice.Sub:
+                    Console.WriteLine("The Subraction of {0} and {1} is : {2}", a, b, a - b);
+                    break;
+                case Choice.Mul:
+                    Console.WriteLine("The Multiplication of {0} and {1} is : {2}", a, b, a * b);
+                    break;
+                case Choice.Div:
+                    Console.WriteLine("The Division of {0} and {1} is : {2}", a, b, a / b);
+                    break;
+                default:
+                    Console.WriteLine("Please select a valid choice");
+                    break;
+            }
+        }
+    }
+
+    class ExecuteCalculator
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the First Number:");
+            int.TryParse(Console.ReadLine(), out int a);
+
+            Console.Write("Enter the Second Number:");
+            int.TryParse(Console.ReadLine(), out int b);
+
+            Console.Write("Enter the Choice:");
+            Enum.TryParse(Console.ReadLine(), true, out Choice c);
+            Calculator calculator = new Calculator(a, b);
+            calculator.Calculate(c);
+        }
+    }
+}
